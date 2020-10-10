e88f13171d6eb814ae6115535cbc493784c51c7f
aQua <6028869+aQuaYi@users.noreply.github.com>
2020-10-10 22:14:12 +0800

    切分函数

--- main.go
+++ main.go
@@ -22,25 +22,23 @@ func main() {
 }
 
 func makeContent() string {
-
 	setDate()
+
+	content := ""
 	//
 	//制作年代表
 	//
-	content := ""
-
 	content += yearTable()
-
+	//
 	// 制作年段落
 	//
 	for year := maxYear - 1; year >= 0; year-- {
 		if !monthHasRecord[year][0] {
 			continue
 		}
-
 		content += yearSection(year)
-
 	}
+
 	return content
 }
 
@@ -158,56 +156,62 @@ func yearSection(year int) string {
 	// add year header
 	content += fmt.Sprintf("\n%s\n\n", yearHeader(year))
 	//add month line
-	content += monthLine(year)
-
+	content += monthesLine(year)
+	//
 	content += fmt.Sprintln()
+
 	for m := 12; m > 0; m-- {
 		if !monthHasRecord[year][m] {
 			continue
 		}
-		content += fmt.Sprintf("\n%s\n\n", monthHeader(year, m))
-		// 输出月历
-		firstDay := time.Date(year, time.Month(m), 1, 0, 0, 0, 0, time.UTC)
-		content += fmt.Sprintln("|周|一|二|三|四|五|六|日|")
-		content += fmt.Sprintln("|:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|")
-		week := 0
-		weekRecord := [8]string{}
-		for day := firstDay; day.Month() == firstDay.Month(); day = day.Add(time.Hour * 24) {
-			_, w := day.ISOWeek()
-			if week != w {
-				week = w
-				weekRecord[0] = fmt.Sprintf("**%d**", week)
-			}
-			d := day.Day()
-			wd := int(day.Weekday())
-			if wd == 0 {
-				wd = 7 //星期天的 weekday 是 0
-			}
-			if dayHasRecord[year][m][d] {
-				weekRecord[wd] = fmt.Sprintf("[[%s\\|%s]]", day.Format(format), fmtNumber(d))
-			} else {
-				weekRecord[wd] = fmt.Sprintf("%s", fmtNumber(d))
-			}
-			if wd == 7 {
-				content += fmt.Sprintf("|%s|\n", strings.Join(weekRecord[:], "|"))
-				weekRecord = [8]string{}
-			}
-		}
-		if weekRecord != [8]string{} {
-			content += fmt.Sprintf("|%s|\n", strings.Join(weekRecord[:], "|"))
-		}
-
-		content += fmt.Sprintf("\n> [年代表](#%%20年代表) - [%d](%s)\n", year, yearHeaderLink(year))
+		content += monthView(year, m)
 	}
 	return content
 }
 
-func monthLine(year int) string {
+func monthesLine(year int) string {
 	content := ""
 	for m := 12; m > 0; m-- {
 		if monthHasRecord[year][m] {
 			content += fmt.Sprintf(" [-%s-](%s)", fmtNumber(m), monthHeaderLink(year, m))
 		}
 	}
+	return content
+}
+
+func monthView(year, month int) string {
+	content := ""
+	content += fmt.Sprintf("\n%s\n\n", monthHeader(year, month))
+	// 输出月历
+	firstDay := time.Date(year, time.Month(month), 1, 0, 0, 0, 0, time.UTC)
+	content += fmt.Sprintln("|周|一|二|三|四|五|六|日|")
+	content += fmt.Sprintln("|:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|")
+	week := 0
+	weekRecord := [8]string{}
+	for day := firstDay; day.Month() == firstDay.Month(); day = day.Add(time.Hour * 24) {
+		_, w := day.ISOWeek()
+		if week != w {
+			week = w
+			weekRecord[0] = fmt.Sprintf("**%d**", week)
+		}
+		d := day.Day()
+		wd := int(day.Weekday())
+		if wd == 0 {
+			wd = 7 //星期天的 weekday 是 0
+		}
+		if dayHasRecord[year][month][d] {
+			weekRecord[wd] = fmt.Sprintf("[[%s\\|%s]]", day.Format(format), fmtNumber(d))
+		} else {
+			weekRecord[wd] = fmt.Sprintf("%s", fmtNumber(d))
+		}
+		if wd == 7 {
+			content += fmt.Sprintf("|%s|\n", strings.Join(weekRecord[:], "|"))
+			weekRecord = [8]string{}
+		}
+	}
+	if weekRecord != [8]string{} {
+		content += fmt.Sprintf("|%s|\n", strings.Join(weekRecord[:], "|"))
+	}
+	content += fmt.Sprintf("\n> [年代表](#%%20年代表) - [%d](%s)\n", year, yearHeaderLink(year))
 	return content
 }
